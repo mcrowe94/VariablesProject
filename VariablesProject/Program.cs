a18fb485b3b0a966a03d6146724d7c44aa443d5d
Monica Crowe <mcrowe9194@gmail.com>
2022-04-01 22:49:25 -0500

    Add project files.

---
+++ VariablesProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariablesProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Monica";
+            int age = 27;
+            char shortName = 'M';
+            bool isMarried = true;
+            double num1 = 4.04;
+            decimal num2 = 4.04m;
+
+            Console.WriteLine($"My name is {name}. I am {age}.");
+        }
+    }
+}
